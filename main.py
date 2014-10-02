9295b10227401fa9b0e0d0a16d9b044ec926a09d
kimlin <kimlindtner@gmail.com>
2014-10-02 16:44:20 +0200

    Lager player

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ __author__ = 'Kim'
 import pygame
 import random
 
+##########################CONSTANTS##################################
+#Skal legges i egen fil senere
 # Define some colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (192, 192, 192)
 
 #Play Size
 BLOCKS = 15  # Should be an odd number, make a try here later!
@@ -28,8 +31,9 @@ SCREEN_HEIGHT = BLOCKS * BLOCK_HEIGHT
 SSBW = 16
 SSBH = 16
 SSM = 1
+######################################################################
 
-# sprite sheet
+# sprite sheet for map
 bomberman_sheet = pygame.image.load("sprite_sheet0.png")
 for i in range(4):
     SPRITE_BLOCK_START = 71
@@ -48,6 +52,53 @@ for i in range(4):
         field = bomberman_sheet.subsurface(bomberman_sheet.get_clip())
         field2x = pygame.transform.scale2x(field)
 
+
+############################## player with sprite sheet:#############################
+######Skal i egen fil ########
+class Player(pygame.sprite.Sprite):
+
+    # vi setter startverdiene til Player:
+    movement_x = 0
+    movement_y = 0
+
+    # Liste med de forskjellige bevegelses bildene, listen er tom nå:
+    walking_frames_d = []
+
+    # Hvilken vei ser spilleren når han starter? D= down
+    direction = "D"
+
+    # Metoder
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        i = 0
+        sheet_start_x = 51
+        sheet_start_y = 38
+        frame_width = 17
+        frame_height = 24
+        sheet_margin = 1
+        sprite_sheet = pygame.image.load("sprite_sheet0.png")
+        # nå laster vi inn alle bildene der bomberman går nedover:
+        for i in range(3):
+            sprite_sheet.set_clip(sheet_start_x + (frame_width+sheet_margin) * i, sheet_start_y, frame_width, frame_height)
+            frame = sprite_sheet.subsurface(sprite_sheet.get_clip())
+            frame2x = pygame.transform.scale2x(frame)
+            self.walking_frames_d.append(frame2x)
+
+        #Set starting frame
+        self.image = self.walking_frames_d[1]
+        # Hides colorkey:
+        self.image.set_colorkey(GRAY)
+
+
+        #Reference to frame rectangle
+        self.rect = self.image.get_rect()
+
+
+        print(type(self.rect))
+        print(type(self.image.get_rect()))
+        print(self.rect)
+
+#####################################################################################
 
 class HardBlock(pygame.sprite.Sprite):
 
@@ -84,6 +135,7 @@ class SoftBlock(pygame.sprite.Sprite):
 # 15x15(BLOCKSxBLOCKS) grid der verdiene = 0, ikke i bruk til nå
 grid = []
 # For hver rekke
+# Grid value 1 = hardblock, 2 = softblock, 3 = empty
 for row in range(BLOCKS):
     # For each row, create a list that will
     # represent an entire row
@@ -108,7 +160,7 @@ all_sprites_list = pygame.sprite.Group()
 hardblock_list = pygame.sprite.Group()
 unmovable_object = HardBlock()
 
-
+# Rammen. De brikkene som ikke kan ødelegges
 for column in range(BLOCKS):
 
     for row in range(BLOCKS):
@@ -139,7 +191,7 @@ for column in range(BLOCKS):
             hardblock_list.add(hardblock)
             grid[row][column] = 1
 
-# soft sprite blocks loads after hardblocks
+# soft sprite blocks loads after hardblocks fordi drid må fylles opp med hardblocks
 softblock_list = pygame.sprite.Group()
 movable_object = SoftBlock()
 
@@ -156,12 +208,23 @@ for column in range(BLOCKS):
                 softblock.rect.y = row * BLOCK_WIDTH
                 all_sprites_list.add(softblock)
                 softblock_list.add(softblock)
+                grid[row][column] = 2
         elif 2 < column < 14 and BLOCKS-column > 3:
             if grid[row][column] == 0:
                 softblock.rect.x = column * BLOCK_HEIGHT
                 softblock.rect.y = row * BLOCK_WIDTH
                 all_sprites_list.add(softblock)
                 softblock_list.add(softblock)
+                grid[row][column] = 2
+        elif grid[row][column] == 0:
+            grid[row][column] = 3
+
+# Spilleren
+player_list = pygame.sprite.Group()
+player = Player()
+player.rect.x = BLOCK_WIDTH
+player.rect.y = BLOCK_HEIGHT
+player_list.add(player)
 
 '''
 softblock.rect.x = column * BLOCK_HEIGHT
@@ -170,14 +233,11 @@ all_sprites_list.add(softblock)
 softblock_list.add(softblock)
 '''
 
-# Set positions of graphics
-background_position = [0, 0]
-# block_position = (71,175,16,16)
-
-
+'''
 # Load and set up graphics.
 player_image = pygame.image.load("player.png").convert()
 player_image.set_colorkey(BLACK)
+'''
 
 done = False
 
@@ -188,9 +248,9 @@ while not done:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             column = mouse_position[0] // BLOCK_WIDTH
             row = mouse_position[1] // BLOCK_HEIGHT
-            print(row)
-            print(column)
-            print("Heavy block = ", grid[row][column])
+            print(row, column)
+            print("Grid value = ", grid[row][column])
+            print(type(player))
 
     mouse_position = pygame.mouse.get_pos()
 
@@ -199,7 +259,7 @@ while not done:
     unmovable_object.rect.x = mouse_position[0]
     unmovable_object.rect.y = mouse_position[1]
     '''
-    # Copy image to screen:
+    ################Her laster vi inn grafikken som ikke er sprites:#####################
 
     for column in range(BLOCKS):
         column_area = 0 < column < BLOCKS-1
@@ -213,10 +273,14 @@ while not done:
         for row in range(2, BLOCKS-1, 2):
             if (column % 2) and column_area:
                 screen.blit(field_shadow2x, [row * BLOCK_WIDTH, column * BLOCK_HEIGHT])
-
+    ####################################################################################
+    # Spritene tegnes etter grafikken som ikke kan røres:
     all_sprites_list.draw(screen)
-    pygame.display.flip()
+    player_list.draw(screen)
 
+    # flip skriver til screen
+    pygame.display.flip()
+    # klokken øker med ett
     clock.tick(60)
 
 

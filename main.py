540963d46dcf288da0700ce7ce9a6b073512a08a
kimlin <kimlindtner@gmail.com>
2014-10-02 03:22:06 +0200

    Laget sprite av soft_block
    laget sprite av soft_block. Startområdet justerer seg dynamisk med
    brettstørrelsen.

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ __author__ = 'Kim'
 """
 
 import pygame
-
+import random
 
 # Define some colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 #Play Size
-BLOCKS = 15
+BLOCKS = 15  # Should be an odd number, make a try here later!
 BLOCK_WIDTH = 32
 BLOCK_HEIGHT = 32
 CAPTION_NAME = "Kim's Bomberman"
@@ -69,10 +69,25 @@ class HardBlock(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
+class SoftBlock(pygame.sprite.Sprite):
+
+    def __init__(self):
+
+    # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+
+        # Load the image
+        self.image = soft_block2x
+
+        # Set our transparent color
+        self.image.set_colorkey(BLACK)
+
+        self.rect = self.image.get_rect()
 
 
 
-# 15x15(BLOCKSxBLOCKS) grid der verdiene = 0
+
+# 15x15(BLOCKSxBLOCKS) grid der verdiene = 0, ikke i bruk til nå
 grid = []
 # For hver rekke
 for row in range(BLOCKS):
@@ -93,11 +108,73 @@ screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption(CAPTION_NAME)
 clock = pygame.time.Clock()
 
-
+# all_sprite_list
 all_sprites_list = pygame.sprite.Group()
-
+# heavy sprite blocks
+hardblock_list = pygame.sprite.Group()
 unmovable_object = HardBlock()
-all_sprites_list.add(unmovable_object)
+
+
+for column in range(BLOCKS):
+
+    for row in range(BLOCKS):
+        # This represents a hardblock
+        hardblock = HardBlock()
+        column_area = 0 < column < BLOCKS-1
+        if not column_area:
+            # Set a random location for the block
+            hardblock.rect.x = column * BLOCK_HEIGHT
+            hardblock.rect.y = row * BLOCK_WIDTH
+            # Add the block to the list of objects
+            all_sprites_list.add(hardblock)
+            hardblock_list.add(hardblock)
+            grid[row][column] = 1
+        elif row == 0 and column_area or row == BLOCKS-1 and column_area:
+            hardblock.rect.x = column * BLOCK_HEIGHT
+            hardblock.rect.y = row * BLOCK_WIDTH
+            all_sprites_list.add(hardblock)
+            hardblock_list.add(hardblock)
+            grid[row][column] = 1
+    for row in range(2, BLOCKS-1, 2):
+        hardblock = HardBlock()
+        column_area = 0 < column < BLOCKS-1
+        if not (column % 2) and column_area:
+            hardblock.rect.x = column * BLOCK_HEIGHT
+            hardblock.rect.y = row * BLOCK_WIDTH
+            all_sprites_list.add(hardblock)
+            hardblock_list.add(hardblock)
+            grid[row][column] = 1
+
+# soft sprite blocks loads after hardblocks
+softblock_list = pygame.sprite.Group()
+movable_object = SoftBlock()
+
+
+for column in range(BLOCKS):
+
+    for row in range(BLOCKS):
+        # This represents a hardblock
+        softblock = SoftBlock()
+        column_area = 0 < column < BLOCKS-1
+        if 2 < row < 14 and BLOCKS-row > 3:
+            if grid[row][column] == 0:
+                softblock.rect.x = column * BLOCK_HEIGHT
+                softblock.rect.y = row * BLOCK_WIDTH
+                all_sprites_list.add(softblock)
+                softblock_list.add(softblock)
+        elif 2 < column < 14 and BLOCKS-column > 3:
+            if grid[row][column] == 0:
+                softblock.rect.x = column * BLOCK_HEIGHT
+                softblock.rect.y = row * BLOCK_WIDTH
+                all_sprites_list.add(softblock)
+                softblock_list.add(softblock)
+
+'''
+softblock.rect.x = column * BLOCK_HEIGHT
+softblock.rect.y = row * BLOCK_WIDTH
+all_sprites_list.add(softblock)
+softblock_list.add(softblock)
+'''
 
 # Set positions of graphics
 background_position = [0, 0]
@@ -107,10 +184,7 @@ background_position = [0, 0]
 
 
 
-
-
 # Load and set up graphics.
-background_image = pygame.image.load("saturn_family1.jpg").convert()
 player_image = pygame.image.load("player.png").convert()
 player_image.set_colorkey(BLACK)
 
@@ -121,18 +195,21 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("Click")
+            column = mouse_position[0] // BLOCK_WIDTH
+            row = mouse_position[1] // BLOCK_HEIGHT
+            print(row)
+            print(column)
+            print("Heavy block = ", grid[row][column])
 
 
+    mouse_position = pygame.mouse.get_pos()
 
 
-
-    # Get the current mouse position. This returns the position
-    # as a list of two numbers.
+    '''
     mouse_position = pygame.mouse.get_pos()
     unmovable_object.rect.x = mouse_position[0]
     unmovable_object.rect.y = mouse_position[1]
-
+    '''
     # Copy image to screen:
 
     for column in range(BLOCKS):
@@ -140,18 +217,13 @@ while not done:
         row_area = 0 < row < BLOCKS-1
 
         for row in range(BLOCKS):
-            if not column_area:
-                screen.blit(hard_block2x, [column*BLOCK_WIDTH,row*BLOCK_HEIGHT])
-            elif row == 0 and column_area or row == BLOCKS-1 and column_area:
-                screen.blit(hard_block2x, [column*BLOCK_WIDTH,row*BLOCK_HEIGHT])
-            elif column == 1 and row_area :
+            if column == 1 and row_area :
                 screen.blit(field_shadow2x, [row*BLOCK_WIDTH,column*BLOCK_HEIGHT])
             else:
                 screen.blit(field2x, [row*BLOCK_WIDTH,column*BLOCK_HEIGHT])
+
         for row in range(2, BLOCKS-1, 2):
-            if not (column % 2) and column_area:
-                screen.blit(hard_block2x, [row*BLOCK_WIDTH,column*BLOCK_HEIGHT])
-            elif (column % 2) and column_area :
+            if (column % 2) and column_area :
                 screen.blit(field_shadow2x, [row*BLOCK_WIDTH,column*BLOCK_HEIGHT])
 
     all_sprites_list.draw(screen)
